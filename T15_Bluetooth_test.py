3285ea6f9f90e02bffea0cda5247e02f39ffce23
ThermitePark <hjp78940@gmail.com>
2026-01-26 11:28:50 +0900

    Testing the Board's Bluetooth with Python

---
+++ T15_Bluetooth_test.py
@@ -0,0 +1,13 @@
+from machine import UART, Pin
+import time
+
+uart1 = UART(0,9600,bits = 8, parity = None, stop = 1, tx = Pin(16), rx = Pin(17))
+try:
+    
+    while True:
+        txData = b'[Hello World]'
+        uart1.write(txData)
+        time.sleep(0.5)
+        
+except KeyboardInterrupt:
+    print("The program has been terminated.")
